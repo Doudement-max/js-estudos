1cac851bbe53f202537c206dd453ed81ba98d01f
doudement-max <pedrodudima>
2021-03-19 09:46:39 -0300

    primeira lista feita

---
+++ exercicios/hora.js
@@ -0,0 +1,4 @@
+var horas=Number(100); 
+var minuto=Number(6000); 
+var dia=Number(4167); 
+console.log('os minutos são equivalente a', minuto, 'que são equivalente a ', horas, ' horas, que dão exatamente', dia);
---
+++ exercicios/percentual.js
@@ -0,0 +1,4 @@
+var bananal=Number(400); 
+var galinhesco=Number(550);
+var maofurada=Number(20); 
+console.log('o partido bananal recebeu ',bananal, 'o partido galinhesco recebeu ',galinhesco,'o partido mão furada recebeu ',maofurada,); 
