9cfdaa7614182b1185635021f6c515e528b6f23a
doudement-max <pedrodudima>
2021-03-24 01:46:27 -0300

    alteraçoes

--- exercicios2/loja.js
+++ exercicios2/loja.js
@@ -1,22 +1,19 @@
-const valor=Number(150); 
-let calculo=0;
- 
-if(valor=>calculo){ 
-    calculo=valor*20/100
-    console.log('se 10 a 19 jogos forem comprados o desconto será',calculo);
+let jogos=1; 
+
+if ( jogos >= 0 && jogos <= 10 ){ 
+  console.log ('não houve desconto')
 } 
-if(valor=>calculo){ 
-    calculo=valor*30/100
-    console.log('se 20 a 49 jogos forem comprados o desconto será',calculo);
+else if(jogos >= 10 && jogos <=19 ){ 
+    console.log ('houve desconto de 20%')  
+}else if(jogos >=20 && jogos <=49){
+    console.log('houve desconto de 30%')
 } 
-if(valor=>calculo){ 
-    calculo=valor*40/100
-    console.log('se 50 a 99 jogos forem comprados o desconto será',calculo);
-}   
- if(valor=>calculo){ 
-    calculo=valor*50/100
-    console.log('se for 100 ou mais jogos comprados o desconto será',calculo); 
-}  
-else{ 
-    console.log('caso seja comprado 9 jogos não terá desconto!');
+else if(jogos >=50 && jogos <=99){ 
+    console.log('houve desconto de 40%') 
+} 
+else if(jogos >=100 ){ 
+    console.log('houve desconto de 50%')
+} 
+else if (jogos < 0) { 
+    console.log('houve um equivoco')
 }
