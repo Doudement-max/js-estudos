ddd678ae06fbb2ee7f4f1061c23cff1097f063a2
doudement-max <pedrodudima>
2021-03-30 09:39:48 -0300

    aulas

---
+++ exercicios2/data.js
@@ -0,0 +1,8 @@
+let dia=2; 
+let mes=10; 
+ 
+
+if ( dia < mes ){ 
+
+    console.log(' data magica 2/ 10/'+dia*mes );
+}
---
+++ exercicios2/operacao.js
@@ -0,0 +1,37 @@
+
+
+console.log('Para fazer calculo de elevaçao ao quadrado digite 1');
+console.log('Para fazer calculo de raiz quadrada digite 2');
+console.log('Para fazer calculo de seno digite 3');
+console.log('Para fazer calculo de exponencial digite 4');
+console.log('sair do programa digite 0');
+
+const  opcao  = Number ('qual opçao  voce deseja: ' );
+
+
+
+let calculo = 0;
+
+if(opcao == 1){
+    let base = Number ( 'qual o numero da base: ' );
+    let expoente = Number ( 'qual o numero do expoente: ' );
+    calculo = Math.pow(base, expoente);
+    console.log(calculo);
+}else if(opcao == 2){
+    let  valor  = Number ( 'qual valor  voce deseja fazer o calculo?: ' );
+    var raizQuadrado = Math.sqrt(valor);
+    console.log(raizQuadrado);
+}else if(opcao == 3){
+    let  valor  = Number ( 'qual valor  voce deseja fazer o calculo?: ' );
+    var  seno = Math.sin(valor);
+    console.log(seno);
+}else if(opcao == 4){
+    let  valor  = Number ( 'qual valor  voce deseja fazer o calculo?: ' );
+    var exponencial = Math.exp(valor);
+    console.log(exponencial);
+}else if(opcao == 0){
+    
+    console.log('Voce saiu do programa');
+}else{
+    console.log('Opaçao não valida')
+}
