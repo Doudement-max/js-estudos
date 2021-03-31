1c00dfc9846317fd9b942695a1f39740c32a8780
doudement-max <pedrodudima>
2021-03-31 19:18:26 -0300

    dia 31 ae

---
+++ exercicios2/retangulo.js
@@ -0,0 +1,9 @@
+console.log(' a altura de um retangulo mede 100 de comprimento'); 
+let base = 2; 
+
+if (base >= 1 && base <= 50){ 
+    console.log('a area e igual');
+} 
+ else if (base <= 900){ 
+ console.log('a area e maior');
+}
